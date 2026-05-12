f5df199f70fbb52b112b8b737537e1d70a6b9704
fuad <fumd03@gmail.com>
2026-05-12 11:11:35 +0300

    configuration

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ from src.train import train
 from src.evaluate import evaluate
 from src.utils import save_model
 
-# Load data
+
 X_train, X_test, y_train, y_test = load_data(
     config.DATA_PATH,
     0.2,
     42
 )
 
-# Build model
+
 model = build_model()
 
-# Train
+
 model = train(model, X_train, y_train)
 
-# Evaluate
+
 acc, report = evaluate(model, X_test, y_test)
 train_acc = model.score(X_train, y_train)
 test_acc = model.score(X_test, y_test)
@@ -28,5 +28,5 @@ print("Test Accuracy:", test_acc)
 print("Accuracy:", acc)
 print("\nReport:\n", report)
 
-# Save model
+
 save_model(model, config.MODEL_PATH)
